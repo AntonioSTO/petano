f7f95c493adcd017d3aa48bb554f8cbf0ec17359
AntonioSTO <antoniostoliveira20@gmail.com>
2024-03-11 21:18:24 -0300

    backend logic completed

--- functions.py
+++ functions.py
@@ -10,14 +10,14 @@ def simulaJogo(time1, time2):
         time1.scoringChance(time2)
     
     for j in range(golsTime2):
-        time1.scoringChance(time1)
+        time2.scoringChance(time1)
 
     print(f'{time1.nome} {time1.gols} x {time2.gols} {time2.nome}')
 
     if time1.gols > time2.gols:
         return time1.nome
     elif time1.gols == time2.gols:
-        return 'Empate!'
+        return 'Empate'
     else:
         return time2.nome
     
---
+++ main.py
@@ -0,0 +1,83 @@
+from teams import times
+from functions import simulaJogo
+from account import Account
+import random
+
+def main():
+    
+    jogador = Account(0)
+
+    while True:
+        print('================================\n' \
+              'Bem-vindo a plataforma PETANO!\n' \
+              'O que deseja fazer?\n' \
+              '1. Depositar\n2. Sacar\n3. Apostar\n4. Saldo\n5. Sair\n' \
+              '================================\n')
+        
+        comando = int(input())
+
+        if comando not in range(1,6):
+            print('Comando inválido. Tente novamente.')
+
+        else:
+            if comando == 1:
+                valor = float(input('Valor a ser depositado: '))
+                jogador.saldo += valor
+                print(f'Novo saldo: R${jogador.saldo}')
+
+            elif comando == 2:
+                valor = float(input('Valor a ser sacado: '))
+                if jogador.saldo >= valor:
+                    jogador.saldo -= valor
+                    print(f'Novo saldo: R${jogador.saldo}')
+                else:
+                    print('Saldo insuficiente.')
+            
+            elif comando == 3:
+                matchUps = times
+                random.shuffle(matchUps)
+
+                bet = float(input('Valor a ser apostado: '))
+
+                if bet <= jogador.saldo:
+                    jogador.saldo -= bet
+
+                    print('Qual jogo deseja apostar?\n' \
+                        f'1. {matchUps[0].nome} x {matchUps[1].nome}\n' \
+                        f'2. {matchUps[2].nome} x {matchUps[3].nome}\n' \
+                        f'3. {matchUps[4].nome} x {matchUps[5].nome}\n')
+                    
+                    escolha = int(input())
+
+
+                    if escolha == 1:
+                        time = input('Resultado: ')
+                        if time == simulaJogo(matchUps[0], matchUps[1]):
+                            print('Você ganhou!!!')
+                            jogador.saldo += 2*bet
+
+                    elif escolha == 2:
+                        time = input('Resultado: ')
+                        if time == simulaJogo(matchUps[2], matchUps[3]):
+                            print('Você ganhou!!!')
+                            jogador.saldo += 2*bet
+
+                    else:
+                        time = input('Resultado: ')
+                        if time == simulaJogo(matchUps[4], matchUps[5]):
+                            print('Você ganhou!!!')
+                            jogador.saldo += 2*bet
+                
+                else:
+                    print('Você não possui esse valor em seu saldo.')
+
+            elif comando == 4:
+                print(f'Saldo: {jogador.saldo}')
+
+            else:
+                print('Saindo...')
+                break
+
+
+if __name__ == '__main__':
+    main()
